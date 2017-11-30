73a782a87a60a44a5f3b837187f1c200be52af4a
calebharnell <calebharnell@hotmail.com>
2017-11-30 14:40:06 +1000

    Update post method

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
-tasks = [
+app.use(bodyParser.json());
+
+let currentKey = 2;
+const genKey = () => ++currentKey;
+
+const tasks = [
   {
     key: 1,
     name: 'Do the washing',
-    date: new Date("October 13, 2014 11:13:00"),
+    date: new Date(),
     complete: false
   },
   {
     key: 2,
     name: 'Walk the dog',
-    date: new Date("October 13, 2014 11:15:00"),
+    date: new Date(),
     complete: false
   }
 ]
 
 app.get('/api/tasks', (req, res) => res.send(tasks))
+
+app.post('/api/tasks', (req, res) => {
+  task = req.body;
+  tasks.push(task);
+  res.send(tasks);
+})
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'))
